16eb9455b2bac14d90a09eee11180d5ce402aebf
Loy Yee Ko <koloyyee@gmail.com>
2020-03-02 09:36:43 +0800

    Updates

--- precache-manifest.280f8b63ad6ae2015e6fd9b0061ac66b.js
+++ precache-manifest.280f8b63ad6ae2015e6fd9b0061ac66b.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e7b9f6776847684f0293f1329badcd7a",
+    "revision": "c76136e7f1f435b3f930884d6cfc6f59",
     "url": "/app-store-hook-infiniteScroll-fetch/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/app-store-hook-infiniteScroll-fetch/static/css/2.47e06e2e.chunk.css"
   },
   {
-    "revision": "09555308c8b77266bff2",
+    "revision": "74e1e3ac2b9bf70649c7",
     "url": "/app-store-hook-infiniteScroll-fetch/static/css/main.96903c40.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/app-store-hook-infiniteScroll-fetch/static/js/2.3c8d8c33.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "09555308c8b77266bff2",
-    "url": "/app-store-hook-infiniteScroll-fetch/static/js/main.9a83efa8.chunk.js"
+    "revision": "74e1e3ac2b9bf70649c7",
+    "url": "/app-store-hook-infiniteScroll-fetch/static/js/main.9eae0b45.chunk.js"
   },
   {
     "revision": "7cbed6a07d215f012e27",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/app-store-hook-infiniteScroll-fetch/precache-manifest.8bb77074a3c15d4cb56f3d9f65419582.js"
+  "/app-store-hook-infiniteScroll-fetch/precache-manifest.280f8b63ad6ae2015e6fd9b0061ac66b.js"
 );
 
 self.addEventListener('message', (event) => {
